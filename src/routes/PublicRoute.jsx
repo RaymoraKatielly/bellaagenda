cf4b5c6e4fff8f0cad7087a494701ea633bb19a4
Ray <raymorakatielly@gmail.com>
2026-03-29 13:59:03 -0300

    Create PublicRoute.jsx

---
+++ src/routes/PublicRoute.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+
+function PublicRoute({ children }) {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return <h1>Carregando...</h1>
+  }
+
+  if (user) {
+    return <Navigate to="/dashboard" />
+  }
+
+  return children
+}
+
+export default PublicRoute
