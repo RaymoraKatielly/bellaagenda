1f5339ee4d77e5aba2d37308a9c2ded6be976fa5
Ray <raymorakatielly@gmail.com>
2026-03-29 12:51:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Toaster } from 'react-hot-toast'
-import AppRoutes from './routes/AppRoutes'
+import { useAuth } from './hooks/useAuth'
 
 function App() {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return <h1>Carregando...</h1>
+  }
+
   return (
-    <>
-      <AppRoutes />
-      <Toaster position="top-right" />
-    </>
+    <div>
+      <h1>BellaAgenda</h1>
+      <p>{user ? `Usuário logado: ${user.email}` : 'Nenhum usuário logado'}</p>
+    </div>
   )
 }
 
