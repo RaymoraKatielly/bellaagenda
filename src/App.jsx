03d58ee11681bb5b840bd061c76c4edf2adcba04
Ray <raymorakatielly@gmail.com>
2026-03-29 13:34:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import Login from './pages/auth/Login'
 import { useAuth } from './hooks/useAuth'
 
 function App() {
-  const { user, loading } = useAuth()
+  const { user, loading, signOut } = useAuth()
+
+  async function handleLogout() {
+    try {
+      await signOut()
+    } catch (error) {
+      console.error('Erro ao sair:', error.message)
+    }
+  }
 
   if (loading) {
     return <h1>Carregando...</h1>
@@ -16,6 +24,8 @@ function App() {
     <div style={{ padding: '2rem' }}>
       <h1>BellaAgenda</h1>
       <p>Usuário logado: {user.email}</p>
+
+      <button onClick={handleLogout}>Sair</button>
     </div>
   )
 }
