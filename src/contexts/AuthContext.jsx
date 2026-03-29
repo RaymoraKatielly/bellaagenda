192cd561c8e6622544c7eb973b02b13b9419c9c8
Ray <raymorakatielly@gmail.com>
2026-03-28 23:38:45 -0300

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react'
-import { supabase } from '../services/supabaseClient'
+import { supabase } from '../services/supabase/client'
 
 export const AuthContext = createContext()
 
